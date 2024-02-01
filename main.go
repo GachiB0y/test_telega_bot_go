6431690584327b7fa8301a00eaf0dee6b17f2df0
AYVolkov <maloletka2222@mail.ru>
2024-02-01 17:05:16 +0300

    add docker compose

--- main.go
+++ main.go
@@ -7,22 +7,7 @@ import (
 	
 )
 
-var (
-	// глобальная переменная в которой храним токен
-	telegramBotToken string
-)
 
-// func init() {
-// 	// принимаем на входе флаг -telegrambottoken
-// 	flag.StringVar(&telegramBotToken, "telegrambottoken", "", "Telegram Bot Token")
-// 	flag.Parse()
-
-// 	// без него не запускаемся
-// 	if telegramBotToken == "" {
-// 		log.Print("-telegrambottoken is required")
-// 		os.Exit(1)
-// 	}
-// }
 
 func main() {
 	// используя токен создаем новый инстанс бота
@@ -40,6 +25,7 @@ func main() {
 	// используя конфиг u создаем канал в который будут прилетать новые сообщения
 	updates, err := bot.GetUpdatesChan(u)
 
+	desiredUserID := 981807694
 	// в канал updates прилетают структуры типа Update
 	// вычитываем их и обрабатываем
 	for update := range updates {
@@ -53,49 +39,21 @@ func main() {
 		// логируем от кого какое сообщение пришло
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-		// chats, err := bot.GetChatAdministrators(tgbotapi.ChatConfig{ChatID: update.Message.Chat.ID})
-        // if err != nil {
-        //     log.Println(err)
-        //     continue
-        // }
-		// for _, chat := range chats {
-        //     chatID := chat.User.ID
-        //     msg := tgbotapi.NewMessage(int64(chatID), messageText)
-        //     _, err := bot.Send(msg)
-        //     if err != nil {
-        //         log.Println(err)
-        //         continue
-        //     }
-		// }
+		
 
 		// List of chat IDs to forward messages to
-		chatIDs := []int64{-1002112299520, -1001993949188,-4195035189 }
+		chatIDs := []int64{-1002003604045,-1002115828429,-1002111555275, }
 
 		// Forward the message to all the chats
 		for _, chatID := range chatIDs {
-			msg := tgbotapi.NewMessage(int64(chatID), messageText)
-		//  msg := tgbotapi.NewForward(chatID, update.Message.Chat.ID, update.Message.MessageID)
-		 bot.Send(msg)
+			if update.Message.From.ID == desiredUserID {
+				msg := tgbotapi.NewMessage(int64(chatID), messageText)
+				//  msg := tgbotapi.NewForward(chatID, update.Message.Chat.ID, update.Message.MessageID)
+				 bot.Send(msg)
+			} 
+
+	
 		}
-		// for update := range updates {
-		// 	chatID := update.Message.Chat.ID
-		// 	chatTitle := update.Message.Chat.Title
-		// 	log.Printf("Chat ID: %d, Chat Title: %s", chatID, chatTitle)
-		// 	continue
-		// }
-
-		// свитч на обработку комманд
-		// комманда - сообщение, начинающееся с "/"
-		// switch update.Message.Command() {
-		// case "start":
-		// 	reply = "Привет. Я телеграм-бот"
-		// case "hello":
-		// 	reply = "world"
-		// }
-
-		// // создаем ответное сообщение
-		// msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
-		// // отправляем
-		// bot.Send(msg)
+	
 	}
 }
